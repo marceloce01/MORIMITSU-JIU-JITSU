270c313b18727051b547fcc43b2836b71679fe9b
Marcelo <marcelowg401pro@gmail.com>
2025-10-21 14:27:33 -0300

    origin and port

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 app.get('/', (req, res) => {
   res.send('Servidor rodando!');
@@ -16,7 +16,9 @@ app.get('/', (req, res) => {
 
 app.use(cors(
     {
-      origin:['*']
+      origin:['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
     }
 ));
 
@@ -25,4 +27,4 @@ app.use(express.json())
 app.use("/user", userRouter)
 app.use("/auth", authRouter)
 
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
+app.listen(PORT, "0.0.0.0", () => console.log(`Servidor rodando na porta ${PORT}`))
