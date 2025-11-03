ea498d8fb325d98bfb48b9008ca427827d978f76
Marcelo <marcelowg401pro@gmail.com>
2025-11-03 17:27:03 -0300

    alter mailer smtp sendgrid

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,10 +5,11 @@ dotenv.config({override: true})
 
 export const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 465,
-    secure: true,
+    port: 2525,
+    secure: false,
+    requireTLS: true,
     auth: {
-        
+
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
 
@@ -19,7 +20,7 @@ export const transporter = nodemailer.createTransport({
 export async function sendMail(to: string, subject: string, html: string){
     try{
         await transporter.sendMail({
-            from: `"Suporte" <${process.env.EMAIL_USER}>`,
+            from: `<${process.env.EMAIL_USER}>`,
             to,
             subject,
             html
