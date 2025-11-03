d6763652f7229e4a87557f350c7871935eee29d8
Marcelo <marcelowg401pro@gmail.com>
2025-11-03 16:01:00 -0300

    alter mailer port

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,16 +5,13 @@ dotenv.config()
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
 
     },
-    tls:{
-        rejectUnauthorized: false
-    }
 })
 
 //Função enviar email ao usuário que solicita criação de conta
