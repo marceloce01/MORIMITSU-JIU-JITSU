c2b27e7be28caef38e7ed2d98ff3b0571e414d29
Marcelo <marcelowg401pro@gmail.com>
2025-11-03 16:09:21 -0300

    mailer dotenv and requireTLS

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,12 +1,13 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 
-dotenv.config()
+dotenv.config({override: true})
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
