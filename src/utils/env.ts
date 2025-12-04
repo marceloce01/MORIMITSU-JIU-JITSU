800c8b31a50d802c79daee0e3602578c4247e1b1
Marcelo <marcelowg401pro@gmail.com>
2025-12-04 19:23:21 -0300

    mailer smtp

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,2 +1,4 @@
 export const EMAIL_USER = process.env.EMAIL_USER
 export const EMAIL_PASS = process.env.EMAIL_PASS
+export const SMTP_HOST  = process.env.SMTP_HOST
+export const SMTP_PORT  = process.env.SMTP_PORT
--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer"
-import { EMAIL_USER, EMAIL_PASS } from "./env.js"
+import { EMAIL_USER, EMAIL_PASS, SMTP_HOST, SMTP_PORT} from "./env.js"
 
 //Função enviar email ao usuário que solicita criação de conta
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth:{
         user: EMAIL_USER,
         pass: EMAIL_PASS
