1310e62a85db8f3e741406ae8a562aa81080636e
Marcelo <marcelowg401pro@gmail.com>
2025-12-10 08:59:26 -0300

    alter classroom repo

--- src/repositories/ClassroomRepository.ts
+++ src/repositories/ClassroomRepository.ts
@@ -2,22 +2,7 @@ import {prisma} from "./prismaClient.js"
 
 export class ClassroomRepository{
     //Criar uma aula na tabela Class
-    static create = async( data: {name: string, image_class_url?: string, teacher_id: string, local?: string}) => {
-       return prisma.class.create({data:{name: data.name, teacher_id: data.teacher_id, local: data.local}})
-    }
-
-    //Atualizar dados de uma aula
-    static update = async(id: string, data: {name?: string, teacher_id?: string, local?: string}) => {
-          return prisma.class.update({where: {id}, data})
-    }
-
-    //Procurar uma aula pelo id
-    static findById = async(id: string) => {
-       return prisma.class.findUnique({where: {id}})
-    }
-
-    //Deletar uma aula
-    static delete = async(id: string)=>{
-        return prisma.class.delete({where: {id}})
+    static create = async( data: {teacher_id: string, class_id: string, classroom_data: string}) => {
+       return prisma.classroom.create({data:{teacher_id: data.teacher_id, class_id: data.class_id, classroom_date: data.classroom_data }})
     }
 }
